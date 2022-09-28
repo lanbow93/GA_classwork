16a3170ac26a2fe5f29abd297797f63c3da7fe13
lanbow93-pi <the.doctor1501@gmail.com>
2022-09-28 16:33:11 -0500

    Street fighter solved other than return value

--- Unit_1/Week_2_Day_3/street_fighter.js
+++ Unit_1/Week_2_Day_3/street_fighter.js
@@ -1,8 +1,33 @@
 class Fighter {
     constructor(name) {
         this.name = name;
-        this.health = Math.floor(Math.random() * (11 - 5) + 5);
-        this.strength = Math.floor(Math.random() * (11 - 5) + 5);
-        this.defense = Math.floor(Math.random() * (11 - 5) + 5);
+        this.health = 10;
+        this.strength = Math.floor(Math.random() * (10 - 5) + 5);
+        this.defense = Math.floor(Math.random() * (6 - 3) + 3);
     }
 }
+let playerOne = new Fighter("Chuck Norris");
+let playerTwo = new Fighter("Bruce Lee");
+
+console.log(playerOne);
+console.log(playerTwo);
+
+const attack = function (fighterOne, fighterTwo) {
+    
+    if(fighterOne.strength > fighterTwo.defense ) {
+        let damage = fighterOne.strength - fighterTwo.defense;
+        console.log(damage)
+        fighterTwo.health = Math.max(0, (fighterTwo.health - damage));
+        console.log(`${fighterOne.name} did damage to ${fighterTwo.name} who is left with ${fighterTwo.health} health`);
+    }
+
+    if(fighterTwo.health === 0) {
+        console.log(fighterOne.name);
+        return fighterOne.name;
+    } else {
+        attack(fighterTwo, fighterOne);
+    }
+
+}
+console.log(attack(playerOne,playerTwo));
+
