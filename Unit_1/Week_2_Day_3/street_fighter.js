b22fe4ba9b9bb9676eea4ab8c67ed7fd3b809668
lanbow93 <the.doctor1501@gmail.com>
2022-10-03 02:01:35 -0400

    completed street_fighter with bells and whistles

--- Unit_1/Week_2_Day_3/street_fighter.js
+++ Unit_1/Week_2_Day_3/street_fighter.js
@@ -3,14 +3,28 @@ class Fighter {
     this.name = name;
     this.health = 10;
     this.strength = Math.floor(Math.random() * (10 - 5) + 5);
-    this.defense = Math.floor(Math.random() * (6 - 3) + 3);
+    this.defense = Math.floor(Math.random() * (10 - 5) + 5);
   }
 }
 //Initializing two fighter objects
 let playerOne = new Fighter("Chuck Norris");
 let playerTwo = new Fighter("Bruce Lee");
 
-// Logging their stats
+// Making sure strength/defense pairings don't cause infinite loop
+let flag = true;
+while (flag) {
+  if (
+    playerOne.strength > playerTwo.defense ||
+    playerTwo.strength > playerOne.defense
+  ) {
+    flag = false;
+  } else {
+    playerOne = new Fighter("Chuck Norris");
+    playerTwo = new Fighter("Bruce Lee");
+  }
+}
+
+// Logging player stats
 console.log(playerOne);
 console.log(playerTwo);
 
@@ -19,17 +33,26 @@ const attack = (fighterOne, fighterTwo) => {
   // Comparing offense and defense and applying proper damage
   if (fighterOne.strength > fighterTwo.defense) {
     let damage = fighterOne.strength - fighterTwo.defense;
-    console.log(damage);
     // If health is knocked lower than 0, set health to 0
     fighterTwo.health = Math.max(0, fighterTwo.health - damage);
     console.log(
-      `${fighterOne.name} did damage to ${fighterTwo.name} who is left with ${fighterTwo.health} health`
+      `    ${fighterOne.name} did ${
+        fighterOne.strength - fighterTwo.defense
+      } damage to ${fighterTwo.name} who is left with ${
+        fighterTwo.health
+      } health`
+    );
+  } else {
+    console.log(
+      `                 ${fighterOne.name} has an attack strengh of ${fighterOne.strength}.\nThey are too weak to take on the the mighty ${fighterTwo.name} with a defene of ${fighterTwo.defense}`
     );
   }
+  console.log(
+    `           ${playerOne.name} Health: ${playerOne.health}   ${playerTwo.name} Health: ${playerTwo.health}\n`
+  );
 
   //If fighter 2 is knocked out, return winners name. Else call function again with fighters reversed.
   if (fighterTwo.health === 0) {
-    console.log(fighterOne.name);
     return fighterOne.name;
   } else {
     return attack(fighterTwo, fighterOne);
@@ -37,4 +60,4 @@ const attack = (fighterOne, fighterTwo) => {
 };
 
 // Calling attack function and logging winners namer
-console.log("The winner is " + attack(playerOne, playerTwo));
+console.log("                      The winner is " + attack(playerOne, playerTwo));
