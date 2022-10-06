9cbe8072a79e044c626837d9c3ff5d5bf0f24619
lanbow93 <the.doctor1501@gmail.com>
2022-10-06 08:09:11 -0400

    After doing Alex's Jquery Masterclass

--- Unit_1/Week_3_Day_3/Intro_to_jquery_1/Alex_Merced_Masterclass/js/app.js
+++ Unit_1/Week_3_Day_3/Intro_to_jquery_1/Alex_Merced_Masterclass/js/app.js
@@ -1,6 +1,37 @@
-console.log(window);
-console.log(document)
-console.log(document.body)
 
+const log = (title, ...message) => {
+    console.log(`===============${title}===================`)
+    console.log(...message)
+    console.log("==========================================")
+}
 
+// Javascript way of changing color of text
+const plain = document.querySelector("#plain");
+// plain.style.color = "pink";
+// Jquery was of changing color of text
+const $jquery = $("#jquery");
+//$jquery.css("color", "red");
 
+// Javascript way of adding classes
+// plain.classList.add("green");
+// Jquery way of adding classes
+// $jquery.addClass("green");
+
+// Adding a button with Javascript
+const button = document.createElement("button");
+button.innerText = "Javascript";
+plain.append(button);
+
+// Adding a button with Jquery
+const $button = $("<button>");
+$button.text("Jquery");
+$jquery.append($button);
+
+// Adding events with Javascript
+button.addEventListener("click", () => {
+    button.style.backgroundColor = "brown";
+})
+// Adding events with Jquery
+$button.on("click", () => {
+    $button.css("background-color", "red");
+})
