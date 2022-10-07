d1e1c94d2d993181cfe996aa1371b23fae772c96
lanbow93 <the.doctor1501@gmail.com>
2022-10-07 18:19:14 -0400

    Add Home works and disables with no elements left

--- Unit_1/Week_3_Day_3/Intro_to_jQuery_2/js/script.js
+++ Unit_1/Week_3_Day_3/Intro_to_jQuery_2/js/script.js
@@ -1,3 +1,10 @@
+var newHomes = [
+    {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
+    {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
+    {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
+    {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
+  ];
+
 $("#addHome").removeClass("btn-danger").addClass("btn-success");
 $("h1").addClass("text-center");
 
@@ -27,3 +34,49 @@ $("#addHome").on("click", function(event) {
     console.log(event);
 })
 
+// Setting a click listener on the entire body
+// Any div with the class circle, will trigger the function
+$("body").on("click", "div.circle", function(){
+    // "this" will be referring to the <div> with a class of circle
+})
+
+// Within the element that has the "homes" ID, 
+// On all tbody elements, set a click listener on the buttons that console logs "this"
+// $("#homes tbody").on("click", "button", function() {
+//    console.log(this);
+    // ^^^ Prints out the specific button's element
+// });
+
+// In an element with the ID homes, locate all elements with tbody
+// When clicking a button element, remove 'this' (the clicked) element's closest <tr> element
+$("#homes tbody").on("click", "button", function() {
+  //  $(this).closest("tr").remove(); // .closest() looks up (parent elements) |  .children() looks down
+    
+  // Same as above but slowly fades out the element before removing it
+  $(this).closest("tr").fadeOut(1000, function() {
+        $(this).remove();
+    })
+});
+
+// var newHomes // {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"}
+let counterAddHome = 0;
+$("#addHome").on("click", function(event) {
+    const currentLink = newHomes[counterAddHome]
+    if (counterAddHome < newHomes.length) {
+        $linkToAdd = $(`
+          <tr>
+            <td>${currentLink.address}</td>
+            <td>${currentLink.sf}</td>
+            <td>${currentLink.bedrooms}</td>
+            <td>${currentLink.baths}</td>
+            <td>${currentLink.price}</td>
+          <td><button class="btn btn-xs btn-danger">Remove</button></td>
+        </tr>
+        `);
+        $("tbody").append($linkToAdd);
+        counterAddHome++;
+    } else {
+        event.currentTarget.disabled = true;
+    }
+    
+})
