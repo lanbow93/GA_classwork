24cec00bc72fc4607f578b295bebcae44eaa6489
lanbow93-pi <the.doctor1501@gmail.com>
2022-10-08 21:57:54 -0500

    first personal attempt at lab

--- Unit_1/Week_3_Day_3/jquery_lab/js/app.js
+++ Unit_1/Week_3_Day_3/jquery_lab/js/app.js
@@ -1,16 +1,42 @@
-$domArray = {
-    skills: ["I know Javascript", "I know HTML"]
+/*
+<body class="centered">
+    <div class="test">
+    <h1>MY DEVELOPER SKILLS</h1>
+    <div class="skillList">
+        <div><input type="button" value="X"> Javascript</div>
+        <div><input type="button" value="X"> HTML</br></div>
+    </div>
+    <form>
+        <input type="text" placeholder="(Developer Skill Here)">
+        <input type="submit" value="Add Skill">
+    </form>
+    </div>*/
+const skillBlock = {
+    devSkills: ["Javascript", "HTML"]
 }
 
-const billboard = $(".skillList")
+const $skillInputs = {
+    textBox: $("input[type='text']"),
+    formField: $("form"),
+    formButton: $("input[type='button']")
+};
 
-/* 
-<div class="skillPair">
-<input type="button" class="skill">
-<label>I know Javascript</label>
-</div> 
-*/
+function skillUpdate(){
+    $(".skillList").empty();
+    for (let skill of skillBlock.devSkills) {
+        $(".skillList").append($(`<div><input type="button" value="X"> ${skill}</div>`))
+    }
+}
 
-function(event) {
+$skillInputs.formField.on("submit", function(event){
+    event.preventDefault();
+    const text = $("input[type='text']")
+    skillBlock.devSkills.push(text.val());
+    text.val("");
+    skillUpdate()
+});
 
-} 
+$skillInputs.formButton.on('click', function(event){
+    const $closestText = $(event.target).text()
+    console.log($closestText)
+;})
