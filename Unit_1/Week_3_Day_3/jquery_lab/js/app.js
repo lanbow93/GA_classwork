82cd59ef1f93cfddea5b8b4413420e4105eb6ca3
lanbow93 <the.doctor1501@gmail.com>
2022-10-10 00:03:35 -0400

    Everything working but local storage

--- Unit_1/Week_3_Day_3/jquery_lab/js/app.js
+++ Unit_1/Week_3_Day_3/jquery_lab/js/app.js
@@ -1,42 +1,53 @@
-/*
-<body class="centered">
-    <div class="test">
-    <h1>MY DEVELOPER SKILLS</h1>
-    <div class="skillList">
-        <div><input type="button" value="X"> Javascript</div>
-        <div><input type="button" value="X"> HTML</br></div>
-    </div>
-    <form>
-        <input type="text" placeholder="(Developer Skill Here)">
-        <input type="submit" value="Add Skill">
-    </form>
-    </div>*/
-const skillBlock = {
-    devSkills: ["Javascript", "HTML"]
-}
+// Step 1: How am I going to keep track of the skills?
+// Array of value to be put on the skillBoard
+const devSkills = ["Cascading Style Sheets (CSS)", "Hypertext Markup Language (HTML)"];
 
-const $skillInputs = {
-    textBox: $("input[type='text']"),
-    formField: $("form"),
-    formButton: $("input[type='button']")
+// Step 2: What are going to be the areas I need to access?
+// A Javascript Object, that holds jQuery key/object selectors
+const $accessPoints = {
+    formArea: $("form"),
+    skillBoard: $(".skillList"),
+    textBox: $("input[type=text]")
 };
 
-function skillUpdate(){
-    $(".skillList").empty();
-    for (let skill of skillBlock.devSkills) {
-        $(".skillList").append($(`<div><input type="button" value="X"> ${skill}</div>`))
+// Step 3: How do I put the skill Array on the board
+// Loops through the Array and appends a new element
+function updateBoard() {
+    $accessPoints.skillBoard.empty(); // Clears out board
+    for (skill of devSkills){ 
+        const nodeToAdd = `<div class="bulletItem"><input type="button" value="X">${skill}</div>`;
+        $accessPoints.skillBoard.append(nodeToAdd);
     }
+
+    // Step 6: How to delete item by clicking on red X
+    // If not in updateBoard, will be canceled out when updateBoard used
+    $("input[type='button']").on("click", function (event) {
+    const $target = $(event.target).parent(); // set to parent
+    const skillToCompare = $target.text();
+    let indexToRemove = devSkills.indexOf(skillToCompare);
+    devSkills.splice(indexToRemove, 1)
+    updateBoard();
+    })
+    
 }
 
-$skillInputs.formField.on("submit", function(event){
-    event.preventDefault();
-    const text = $("input[type='text']")
-    skillBlock.devSkills.push(text.val());
-    text.val("");
-    skillUpdate()
-});
+// Step 4: How to grab info from textbox and put it in array
+// Store text then clears the box
+function addToList() {
+    let boxValue = $accessPoints.textBox.val();
+    devSkills.push(boxValue);
+    $accessPoints.textBox.val("");
+}
 
-$skillInputs.formButton.on('click', function(event){
-    const $closestText = $(event.target).text()
-    console.log($closestText)
-;})
+// Step 5: How to get it to add and update when the button is clicked
+$accessPoints.formArea.on("submit", function(event){
+    event.preventDefault(); // Prevent page refresh
+    addToList();
+    updateBoard();
+})
+
+updateBoard(); // Needed for deleting without adding first
+
+
+
+
