856d2471f74200af29ecbbcced3ac2b5a8188e26
lanbow93 <the.doctor1501@gmail.com>
2022-11-03 21:19:10 -0400

    Tip Calculator express application created.

--- Unit_2/Week_7_Day_2/express-lab-2/server.js
+++ Unit_2/Week_7_Day_2/express-lab-2/server.js
@@ -10,6 +10,10 @@ app.get("/greeting/:name", (request, response) => {
     response.send(`Howdy there, ${request.params.name}! It's great to see you!`)
 })
 
+app.get("/tip/:total/:tipPercentage", (request, response) => {
+    response.send(`${request.params.total * (request.params.tipPercentage * .01)}`)
+})
+
 app.listen(PORT, (request, response) => {
     console.log("Listening on port: ", PORT)
 }); 
