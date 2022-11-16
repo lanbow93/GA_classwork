c2811453224bbd6a0a2f950fe43623560bd21586
lanbow93 <the.doctor1501@gmail.com>
2022-11-16 16:10:55 -0600

    completed budget with balance that changes color

--- Unit_2/Week_8_Day_2/Budgtr/controllers/budget.js
+++ Unit_2/Week_8_Day_2/Budgtr/controllers/budget.js
@@ -1,18 +1,39 @@
+const { parse } = require("dotenv");
 const express = require("express");
 const router = express.Router();
 const Budget = require("../models/budget")
 
+
+
+
+
 router.get("/", (req, res) => {
-    res.render("budget/index.ejs", {
-        Budget: Budget
+    let bankAccount = 0;
+    Budget.forEach((item) => {
+        bankAccount += parseInt(item.amount)
     })
+    
+
+    res.render("budget/index.ejs", {
+        Budget: Budget,
+        Account: bankAccount
+    })
+})
+
+router.get("/new", (req, res) => {
+    res.render("budget/new.ejs")
+})
+
+router.post("/", (req, res) => {
+    req.body.tags = req.body.tags ? req.body.tags : ""
+    Budget.push(req.body)
+    res.redirect("/budgets")
 })
 
 router.get("/:id", (req, res) => {
     res.render("budget/show.ejs", {
         item: Budget[req.params.id],
         index: req.params.id
-
     } )
 })
 
