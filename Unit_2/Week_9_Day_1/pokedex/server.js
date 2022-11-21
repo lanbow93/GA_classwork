bae36bf5a7003fe1cee8aecc148bae493eec3787
lanbow93 <the.doctor1501@gmail.com>
2022-11-21 13:11:57 -0600

    pokemon server outline built and operational

---
+++ Unit_2/Week_9_Day_1/pokedex/server.js
@@ -0,0 +1,29 @@
+// Import Dependencies
+const express = require("express");
+const PokemonRouter = require("./controllers/pokemon");
+const methodOverride = require("method-override");
+const morgan = require("morgan")
+require("dotenv").config()
+
+// Global Variables
+const PORT = process.env.PORT
+
+// Generating app object
+app = express();
+
+// Middleware
+app.use(methodOverride("_method"));
+app.use("/static", express.static("public"));
+app.use(morgan("tiny"))
+app.use(express.urlencoded({extended: true})) // parse HTML form data 
+app.use("/pokemon", PokemonRouter)
+
+// Route to test server
+app.get("/", (req, res) => {
+    res.send("Pokemon Server is functional and running")
+})
+
+// App listener
+app.listen(PORT, (req, res) => {
+    console.log(`Listening on port: ${PORT}`);
+})
