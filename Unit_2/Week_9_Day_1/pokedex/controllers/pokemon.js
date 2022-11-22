e98a2012bf46a8f4d2dbce3d1bf910173092daae
lanbow93 <the.doctor1501@gmail.com>
2022-11-22 14:44:31 -0600

    Pokemon partiall appear, unable to function

--- Unit_2/Week_9_Day_1/pokedex/controllers/pokemon.js
+++ Unit_2/Week_9_Day_1/pokedex/controllers/pokemon.js
@@ -1,8 +1,40 @@
 const express = require("express");
+const pokemon = require("../models/pokemon");
+const Pokemon = require("../models/pokemon")
 const router = express.Router();
+let pageCounter = {
+    page: 0,
+    forward: function() { 
+        if(this.page = Math.ceil(Pokemon.length /6)) {
+            this.page = 0
+            console.log(this.page)
+        } else {
+            this.page += 1;
+            console.log(this.page)
+        }
+    },
+    backward: function() {
+        if (this.page = 0) {
+            this.page = Math.ceil(Pokemon.length /6);
+            console.log(this.page)
+        } else {
+            this.page -= 1
+            console.log(this.page)
+        }
+    },
+    reset: function() { 
+        this.page = 0
+        console.log(this.page)
+    }
+}
+
+pageCounter.forward
 
 router.get("/", (req, res) => {
-    res.render("pokemon/index.ejs")
+    res.render("pokemon/index.ejs", {
+        AllPokemon: Pokemon,
+        pageTracker: pageCounter
+    })
 })
 
 
