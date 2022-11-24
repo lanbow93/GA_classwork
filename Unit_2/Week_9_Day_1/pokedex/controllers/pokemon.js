431d214e8827b2ae08f0cbf7fa65027cf018ec12
lanbow93 <the.doctor1501@gmail.com>
2022-11-23 18:36:31 -0600

    Forward and backward buttons work, error on 26

--- Unit_2/Week_9_Day_1/pokedex/controllers/pokemon.js
+++ Unit_2/Week_9_Day_1/pokedex/controllers/pokemon.js
@@ -2,38 +2,15 @@ const express = require("express");
 const pokemon = require("../models/pokemon");
 const Pokemon = require("../models/pokemon")
 const router = express.Router();
-let page = 0
-const pageCounter = {
-    forward: function() { 
-        if(page === Math.ceil(Pokemon.length /6)) {
-            page = 0
-            console.log(page)
-        } else {
-            page += 1;
-            console.log(page)
-        }
-    },
-    backward: function() {
-        if (page === 0) {
-            page = Math.ceil(Pokemon.length /6);
-            console.log(page)
-        } else {
-            page -= 1
-            console.log(page)
-        }
-    },
-    reset: function() { 
-        page = 0
-        console.log(page)
-    }
-}
 
 router.get("/", (req, res) => {
+    const pageNumber = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
+    console.log(pageNumber)
     res.render("pokemon/index.ejs", {
         AllPokemon: Pokemon,
-        pageTracker: pageCounter,
-        page: page
+        page: pageNumber
     })
+    
 })
 
 
