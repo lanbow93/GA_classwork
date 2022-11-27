49812af49b487378484558a932cb45964169a08a
lanbow93 <the.doctor1501@gmail.com>
2022-11-27 14:16:22 -0600

    updates

--- Unit_2/Week_9_Day_1/pokedex/controllers/pokemon.js
+++ Unit_2/Week_9_Day_1/pokedex/controllers/pokemon.js
@@ -5,10 +5,15 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     const pageNumber = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
-    console.log(pageNumber)
     res.render("pokemon/index.ejs", {
         AllPokemon: Pokemon,
         page: pageNumber
+    })
+})
+
+router.get("/:id", (req, res) => {
+    res.render("pokemon/show.ejs", {
+        Pokemon: Pokemon[req.params.id]
     })
 })
 
