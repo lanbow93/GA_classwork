9f26cbd8bc503c27ed7dab310c06c4c73f74c433
lanbow93 <the.doctor1501@gmail.com>
2022-11-28 19:40:17 -0600

    Show route added, links go to proper id page

--- Unit_2/Week_9_Day_3/animals/controllers/animal.js
+++ Unit_2/Week_9_Day_3/animals/controllers/animal.js
@@ -9,6 +9,22 @@ function errorCatcher(error, res) {
     res.json(error)
 }
 
+// SEED Route
+router.get("/seed", async (req, res) => {
+    await Animal.remove({}).catch((error => errorHandler(error, res)))
+    const animals = await Animal.create([
+        {species: "chimpanzee", extinct: false, location: "West Africa", lifeExpectancy: 50},
+        {species: "dodo", extinct: true, location: "Mauritius", lifeExpectancy: 30},
+        {species: "panda", extinct: false, location: "China", lifeExpectancy: 20},
+    ])
+    res.json(animals)
+})
+
+// Index route
+router.get("/", async (req, res) => {
+    const animals = await Animal.find({}).catch((error => errorHandler(error, res)))
+    res.render("animals/index.ejs", {animals})
+})
 
 
 
