d7e14d6613e048be9422114ed768cd78c9be2ba0
lanbow93 <the.doctor1501@gmail.com>
2022-12-23 16:55:37 -0600

    button spaced properly

--- Unit_3/Week_14_Day_1/korilla_receipts/src/components/Receipts.js
+++ Unit_3/Week_14_Day_1/korilla_receipts/src/components/Receipts.js
@@ -6,12 +6,11 @@ export default function Receipts(props) {
     const handleEvent = (event) => {
         const newState = [...props.orderInformation]
         newState.sort((before, after) => before.person !== after.person ? before.person < after.person ? -1 : 1 : 0);
-        console.log(newState)
         props.setOrderInformation(newState)
     }
 
     return <>
-
+        <br/>
         <button onClick={handleEvent}>Sort</button>
         <div className="receiptArea">
             {props.orderInformation.map((order) => <Receipt order={order} key={order.id}/>)}
