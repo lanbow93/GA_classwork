36aa3b7f8ac3b63a4776c8ed23f7a8fb4b02715a
lanbow93 <the.doctor1501@gmail.com>
2023-02-01 08:51:00 -0600

    initial game variables defined

---
+++ Unit_4/Week_20_Day_1/landscaper.py
@@ -0,0 +1,8 @@
+menu = "Main Menu Selection\n1. Mow Lawn  2. Visit Store  3. View Inventory  4. Print Options"
+store_menu = "Please select from the following options:\n"
+user_stats = {'tool': 'teeth', 'profit': 1, 'cash': 0, }
+tool_options = { "Rusty Scissors": 5, "Old-Timey Push Lawnmower": 50, "Fancy Battery-Powered Lawnmower": 100, "Team of Starving Students": 250 }
+tool_cost = { "Rusty Scissors": 5, "Old-Timey Push Lawnmower": 25, "Fancy Battery-Powered Lawnmower": 250, "Team of Starving Students": 500 }
+
+
+
