1f335e2911ad9b133b6b369b4f8e65b4a8a813e4
lanbow93 <the.doctor1501@gmail.com>
2023-02-09 13:24:30 -0600

    .

--- Unit_4/Week_21_Day_1/answers.sql
+++ Unit_4/Week_21_Day_1/answers.sql
@@ -23,14 +23,15 @@ SELECT * FROM city WHERE countrycode ILIKE 'SMR';
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 SELECT countrycode FROM city WHERE name ILIKE 'Serra%' AND name != 'Serravalle';
--- Serra | BRA
+--  BRA
 SELECT * FROM country WHERE code iLIKE 'bra';
-
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
 
 SELECT * FROM city WHERE id='211';
+-- Brasillia
+
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
 
@@ -44,7 +45,7 @@ SELECT * FROM city WHERE id='211';
 -- In a city of ninety-one thousand and now, eighty five.
 
 SELECT * FROM city WHERE population=91084;
-
+--Santa Monica
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
